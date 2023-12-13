81617acae16dc0872e9b7bc1baafc7c19f33967e
Kristina Dedova <dan005bk@gmail.com>
2023-12-14 00:13:49 +0300

    tested user auth

--- app/src/main/java/com/unoth/messenger/MainActivity.java
+++ app/src/main/java/com/unoth/messenger/MainActivity.java
@@ -1,13 +1,44 @@
 package com.unoth.messenger;
 
+import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword("pezduk@gamil.com", "qwerty")
+                .addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        FirebaseUser user = auth.getCurrentUser();
+                        if (user != null) {
+                            Log.d(TAG, "User authorized: " + user.getEmail());
+                        } else {
+                            Log.d(TAG, "User not authorized");
+                        }
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, e.toString());
+                    }
+                });
+
+
     }
 }
