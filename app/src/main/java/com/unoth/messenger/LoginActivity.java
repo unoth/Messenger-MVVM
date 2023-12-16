9ef0998958e153136adb88085723d426856dbde7
Kristina Dedova <dan005bk@gmail.com>
2023-12-16 08:30:50 +0300

    added LoginViewModel

--- app/src/main/java/com/unoth/messenger/LoginActivity.java
+++ app/src/main/java/com/unoth/messenger/LoginActivity.java
@@ -5,8 +5,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,19 +19,25 @@ public class LoginActivity extends AppCompatActivity {
     private Button buttonLogin;
     private TextView textViewForgotPassword;
     private TextView textViewRegister;
+    private LoginViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initViews();
+        viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        observeViewModel();
+        setupClickListener();
+    }
 
+    private void setupClickListener() {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = editTextEmail.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
-                // login logic
+                viewModel.login(email, password);
             }
         });
         textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +55,26 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = RegistrationActivity.newIntent(LoginActivity.this);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String error) {
+                if (error != null) {
+                    Toast.makeText(LoginActivity.this, error, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        viewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser != null) {
+                    //launch user screen
+                    Toast.makeText(LoginActivity.this, "user auth", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
---
+++ app/src/main/java/com/unoth/messenger/LoginViewModel.java
@@ -0,0 +1,46 @@
+package com.unoth.messenger;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginViewModel extends ViewModel {
+
+    private FirebaseAuth auth;
+
+    private MutableLiveData<String> error = new MutableLiveData<>();
+    private MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
+
+    public LiveData<FirebaseUser> getUser() {
+        return user;
+    }
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    public LoginViewModel() {
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void login(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        user.setValue(authResult.getUser());
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        error.setValue(e.getMessage());
+                    }
+                });
+    }
+}
