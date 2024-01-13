efe3042cc0b6c566a09cd80534811fdb7c868a69
Kristina Dedova <dan005bk@gmail.com>
2024-01-14 02:48:23 +0300

    fixed Messages Adapter

--- app/src/main/java/com/unoth/messenger/ChatActivity.java
+++ app/src/main/java/com/unoth/messenger/ChatActivity.java
@@ -1,5 +1,7 @@
 package com.unoth.messenger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -7,20 +9,52 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
+    private static final String EXTRA_CURRENT_USER_ID = "current_id";
+    private static final String EXTRA_OTHER_USER_ID = "other_id";
 
     private TextView textViewTitle;
     private View onlineStatus;
     private RecyclerView recyclerViewMessage;
     private EditText editTextMessage;
     private ImageView imageViewSendMessage;
+    private MessageAdapter messageAdapter;
+    private String currentUserId;
+    private String otherUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         initViews();
+
+        currentUserId = getIntent().getStringExtra(EXTRA_CURRENT_USER_ID);
+        otherUserId = getIntent().getStringExtra(EXTRA_OTHER_USER_ID);
+        messageAdapter = new MessageAdapter(currentUserId);
+
+        //testing
+        recyclerViewMessage.setAdapter(messageAdapter);
+        List<Message> messageList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Message message = new Message(
+                    currentUserId,
+                    otherUserId,
+                    "text " + i
+            );
+            messageList.add(message);
+        }
+        for (int i = 0; i < 10; i++) {
+            Message message = new Message(
+                    otherUserId,
+                    currentUserId,
+                    "text " + i
+            );
+            messageList.add(message);
+        }
+        messageAdapter.setMessages(messageList);
     }
 
     private void initViews() {
@@ -29,5 +63,12 @@ public class ChatActivity extends AppCompatActivity {
         recyclerViewMessage = findViewById(R.id.recyclerViewMessage);
         editTextMessage = findViewById(R.id.editTextMessage);
         imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+    }
+
+    public static Intent newIntent(Context context, String currentUserId, String otherUserId) {
+        Intent intent = new Intent(context, ChatActivity.class);
+        intent.putExtra(EXTRA_OTHER_USER_ID, otherUserId);
+        intent.putExtra(EXTRA_CURRENT_USER_ID, currentUserId);
+        return intent;
     }
 }
